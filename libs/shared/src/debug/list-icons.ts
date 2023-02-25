421ab5a6d3d22363ca41fea8004a641457b55b00
Pithaya <19533412+Pithaya@users.noreply.github.com>
2023-02-25 11:50:25 +0100

    feat(dev): Add new dev extension to list icons

--- libs/shared/src/debug/list-icons.ts
+++ libs/shared/src/debug/list-icons.ts
@@ -2,12 +2,14 @@
  * List all the existing icons in the context menu.
  */
 export function listIcons(): void {
-    for (const [iconName, icon] of Object.entries(Spicetify.SVGIcons)) {
-        new Spicetify.ContextMenu.Item(
+    const items = Object.entries(Spicetify.SVGIcons).map(([iconName, icon]) => {
+        return new Spicetify.ContextMenu.Item(
             iconName,
             () => {},
             () => true,
             `<svg height="16" width="16" viewBox="0 0 16 16" fill="currentColor">${icon}</svg>` as any
-        ).register();
-    }
+        );
+    });
+
+    new Spicetify.ContextMenu.SubMenu('Icons', items, () => true).register();
 }
