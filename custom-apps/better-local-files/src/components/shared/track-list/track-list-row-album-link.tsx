705568587bc7a1cadf9a8f9a5a0aeb598bd3c8a7
Pithaya <19533412+Pithaya@users.noreply.github.com>
2023-03-10 00:03:50 +0100

    fix(local-files): Remove unused menu

--- custom-apps/better-local-files/src/components/shared/track-list/track-list-row-album-link.tsx
+++ custom-apps/better-local-files/src/components/shared/track-list/track-list-row-album-link.tsx
@@ -10,26 +10,15 @@ interface TrackListRowAlbumLinkProps {
 
 export function TrackListRowAlbumLink(props: TrackListRowAlbumLinkProps) {
     return (
-        <Spicetify.ReactComponent.RightClickMenu
-            menu={
-                // TODO: Album menu
-                <Spicetify.ReactComponent.Menu>
-                    <Spicetify.ReactComponent.MenuItem>
-                        <span>Album menu</span>
-                    </Spicetify.ReactComponent.MenuItem>
-                </Spicetify.ReactComponent.Menu>
-            }
+        <a
+            draggable="true"
+            className="standalone-ellipsis-one-line"
+            dir="auto"
+            href="#"
+            tabIndex={-1}
+            onClick={() => navigateTo(Routes.album, props.track.album.uri)}
         >
-            <a
-                draggable="true"
-                className="standalone-ellipsis-one-line"
-                dir="auto"
-                href="#"
-                tabIndex={-1}
-                onClick={() => navigateTo(Routes.album, props.track.album.uri)}
-            >
-                {props.track.album.name}
-            </a>
-        </Spicetify.ReactComponent.RightClickMenu>
+            {props.track.album.name}
+        </a>
     );
 }
