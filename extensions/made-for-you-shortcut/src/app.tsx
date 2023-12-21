b7e92b978552bc59c11366d2344419c64ad2da25
Pithaya <19533412+Pithaya@users.noreply.github.com>
2023-12-21 19:00:16 +0100

    refactor(made-for-you): Fix lint issues

--- extensions/made-for-you-shortcut/src/app.tsx
+++ extensions/made-for-you-shortcut/src/app.tsx
@@ -1,7 +1,11 @@
+import { NavBarLink } from '@shared/components/nav-bar-link.component';
 import { waitForElement, waitForSpicetify } from '@shared/utils';
 import i18next from 'i18next';
+import { Crown } from 'lucide-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-async function main() {
+async function main(): Promise<void> {
     await waitForSpicetify();
 
     const locale: typeof Spicetify.Locale = Spicetify.Locale;
@@ -27,18 +31,36 @@ async function main() {
     const element = await waitForElement('#spicetify-sticky-list');
 
     // Prevent global changes to the stroke-width distorting the icon
-    let styles = `
+    const styles = `
         svg.made-for-you-icon {
             stroke-width: 2px !important;
         }
     `;
 
-    let styleSheet = document.createElement('style');
+    const styleSheet = document.createElement('style');
     styleSheet.innerText = styles;
     document.head.appendChild(styleSheet);
 
-    const reactHelper = await import('./helpers/react-helper');
-    reactHelper.ReactHelper.registerNavBarLink(element);
+    ReactDOM.render(
+        ReactDOM.createPortal(
+            <NavBarLink
+                icon={
+                    <Crown size={24} className="made-for-you-icon home-icon" />
+                }
+                activeIcon={
+                    <Crown
+                        size={24}
+                        fill="currentColor"
+                        className="made-for-you-icon home-active-icon"
+                    />
+                }
+                label={i18next.t('forYou')}
+                href="/genre/made-for-x-hub"
+            />,
+            element,
+        ),
+        document.createElement('div'),
+    );
 }
 
 export default main;
