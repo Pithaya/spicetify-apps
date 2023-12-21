0205300ce214e9ff07b0ed21f971758a120d4da6
Pithaya <19533412+Pithaya@users.noreply.github.com>
2023-12-21 19:08:35 +0100

    refactor(dev-extensions): Remove extension and fix lint issues

--- dev-extensions/list-icons/src/app.tsx
+++ dev-extensions/list-icons/src/app.tsx
@@ -1,7 +1,7 @@
 import { listIcons } from '@shared/debug';
 import { waitForSpicetify } from '@shared/utils';
 
-async function main() {
+async function main(): Promise<void> {
     await waitForSpicetify();
 
     listIcons();
