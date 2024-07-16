f23e33f830e73fafc071f58933f43d21e45e0f5d
Pithaya <19533412+Pithaya@users.noreply.github.com>
2024-07-16 11:44:54 +0200

    feat(playlist-maker): Add cache

--- custom-apps/playlist-maker/src/models/nodes/filter/genre-processor.ts
+++ custom-apps/playlist-maker/src/models/nodes/filter/genre-processor.ts
@@ -7,19 +7,25 @@ import type { Artist } from '@spotify-web-api';
 import genresJson from 'custom-apps/playlist-maker/src/assets/genres.json';
 import { wait } from '@shared/utils/promise-utils';
 import { getSdkClient } from '@shared/utils/web-api-utils';
+import {
+    createWithExpiry,
+    getArtistsGenresCache,
+    removeExpired,
+    setArtistsGenresCache,
+} from 'custom-apps/playlist-maker/src/utils/storage-utils';
 
 const genres: Record<string, string[]> = genresJson;
 
+// TODO: Add this to settings
+export const artistGenresStoreTime = 1000 * 60 * 60 * 24 * 10; // 10 days
+
+// FIXME: Web api always return an empty array for genres ?
+
 export type GenreFilterData = BaseNodeData & {
-    genres: string[];
+    genreCategories: string[];
 };
 
 export class GenreProcessor extends NodeProcessor<GenreFilterData> {
-    private static readonly artistsGenres: Map<string, string[]> = new Map<
-        string,
-        string[]
-    >();
-
     protected override async getResultsInternal(
         input: Track[],
     ): Promise<Track[]> {
@@ -33,18 +39,24 @@ export class GenreProcessor extends NodeProcessor<GenreFilterData> {
                 ),
         );
 
-        await this.getArtistsGenres(tracks);
+        const artistGenres = await this.getArtistsGenres(tracks);
+
+        const genresToKeep = new Set<string>();
+        for (const genreCategory of this.data.genreCategories) {
+            genres[genreCategory]
+                .filter((genre) => !genre.startsWith('---'))
+                .forEach((genre) => genresToKeep.add(genre));
+        }
 
         for (const track of tracks) {
             const trackGenres = track.artists.flatMap(
-                (artist) => GenreProcessor.artistsGenres.get(artist.uri) ?? [],
+                (artist) => artistGenres.get(artist.uri) ?? [],
             );
 
-            for (const genre of this.data.genres) {
-                if (trackGenres.includes(genre)) {
-                    result.push(track);
-                    break;
-                }
+            if (
+                trackGenres.some((trackGenre) => genresToKeep.has(trackGenre))
+            ) {
+                result.push(track);
             }
         }
 
@@ -55,11 +67,15 @@ export class GenreProcessor extends NodeProcessor<GenreFilterData> {
      * Get artists genres and add them to the cache.
      * @param tracks Tracks to process.
      */
-    private async getArtistsGenres(tracks: Track[]): Promise<void> {
+    private async getArtistsGenres(
+        tracks: Track[],
+    ): Promise<Map<string, string[]>> {
+        const artistCache = removeExpired(getArtistsGenresCache());
+
         const sdk = getSdkClient();
         const artists = tracks
             .flatMap((track) => track.artists)
-            .filter((artist) => !GenreProcessor.artistsGenres.has(artist.uri));
+            .filter((artist) => !artistCache.has(artist.uri));
         const chunks = splitInChunks(artists, MAX_GET_MULTIPLE_ARTISTS_IDS);
 
         const artistsData: Artist[] = [];
@@ -76,35 +92,14 @@ export class GenreProcessor extends NodeProcessor<GenreFilterData> {
         }
 
         for (const artistData of artistsData) {
-            const artistSubGenres = artistData.genres;
-
-            if (artistSubGenres.length === 0) {
-                GenreProcessor.artistsGenres.set(artistData.uri, []);
-                continue;
-            }
-
-            const artistGenres = new Set<string>();
-
-            for (const subgenre of artistSubGenres) {
-                const genre: [string, string[]] | undefined = Object.entries(
-                    genres,
-                ).find(([genre, subgenres]) => subgenres.includes(subgenre));
-
-                if (genre === undefined) {
-                    console.warn(
-                        'Could not find genre for subgenre:',
-                        subgenre,
-                    );
-                    continue;
-                }
-
-                artistGenres.add(genre[0]);
-            }
-
-            GenreProcessor.artistsGenres.set(
+            artistCache.set(
                 artistData.uri,
-                Array.from(artistGenres),
+                createWithExpiry(artistData.genres, artistGenresStoreTime),
             );
         }
+
+        setArtistsGenresCache(artistCache);
+
+        return new Map(Array.from(artistCache).map(([k, v]) => [k, v.value]));
     }
 }
