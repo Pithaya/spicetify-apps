d190485c8c060bcb8cef75f01980602e58f9f824
Pithaya <19533412+Pithaya@users.noreply.github.com>
2024-11-03 15:51:11 +0100

    fix(eternal-jukebox): fix playbar button style and label

--- custom-apps/eternal-jukebox/src/components/PlaybarButton.tsx
+++ custom-apps/eternal-jukebox/src/components/PlaybarButton.tsx
@@ -11,20 +11,44 @@ export function PlaybarButton(): JSX.Element {
         await window.jukebox.setEnabled(!window.jukebox.isEnabled);
     }
 
+    const label = isActive ? 'Disable jukebox' : 'Enable jukebox';
+    const style: React.CSSProperties = {
+        color: isActive ? 'var(--spice-button-active)' : 'inherit',
+    };
+    const beforeStyle: React.CSSProperties = {
+        backgroundColor: 'currentcolor',
+        borderRadius: '50%',
+        bottom: '0',
+        display: 'block',
+        left: '50%',
+        position: 'absolute',
+        width: '4px',
+        inlineSize: '4px',
+        height: '4px',
+        transform: 'translateX(-50%)',
+    };
+
     return (
         <Spicetify.ReactComponent.TooltipWrapper
-            label={'Enable jukebox'}
+            label={label}
             showDelay={100}
             renderInline={false}
         >
-            <button
-                className={`main-repeatButton-button ${
-                    isActive ? 'main-repeatButton-active' : ''
-                }`}
-                onClick={toggleJukebox}
-            >
-                <Infinity size={24} />
-            </button>
+            <Spicetify.ReactComponent.ButtonTertiary
+                aria-label={label}
+                buttonSize={'sm'}
+                onClick={async (e: any) => {
+                    e.stopPropagation();
+                    await toggleJukebox();
+                }}
+                iconOnly={() => (
+                    <>
+                        <Infinity size={24} />
+                        {isActive && <div style={beforeStyle}></div>}
+                    </>
+                )}
+                style={style}
+            ></Spicetify.ReactComponent.ButtonTertiary>
         </Spicetify.ReactComponent.TooltipWrapper>
     );
 }
