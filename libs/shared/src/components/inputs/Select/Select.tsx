a95b64ca0defe14f4c8248d5717ae867edaf151e
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-01-19 16:38:08 +0100

    feat(shared): update select component

--- libs/shared/src/components/inputs/Select/Select.tsx
+++ libs/shared/src/components/inputs/Select/Select.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import styles from './Select.module.scss';
-import { ChevronDown } from 'lucide-react';
 
 type Item = {
     value: string;
@@ -15,56 +13,33 @@ export type Props = {
     disabled?: boolean;
 };
 
-function SelectMenu(
-    props: Readonly<Pick<Props, 'items' | 'onItemClicked'>>,
-): JSX.Element {
-    return (
-        <Spicetify.ReactComponent.Menu
-            className={'main-contextMenu-menu' + ' ' + styles['menu']}
-        >
-            {props.items.map((item) => (
-                <Spicetify.ReactComponent.MenuItem
-                    key={item.value}
-                    onClick={() => {
-                        props.onItemClicked(item);
-                    }}
-                >
-                    <span>{item.label}</span>
-                </Spicetify.ReactComponent.MenuItem>
-            ))}
-        </Spicetify.ReactComponent.Menu>
-    );
-}
-
 export function Select(props: Readonly<Props>): JSX.Element {
     return (
-        <Spicetify.ReactComponent.ContextMenu
-            trigger="click"
-            action="toggle"
-            placement="bottom"
-            menu={
-                <SelectMenu
-                    items={props.items}
-                    onItemClicked={props.onItemClicked}
-                />
-            }
+        <select
+            className="main-dropDown-dropDown"
+            disabled={props.disabled}
+            value={props.selectedValue ?? undefined}
+            onChange={(event) => {
+                const item = props.items.find(
+                    (i) => i.value === event.target.value,
+                );
+
+                if (item) {
+                    props.onItemClicked(item);
+                }
+            }}
         >
-            <div
-                className={`main-dropDown-dropDown ${styles['dropdown']} nodrag`}
-                style={{
-                    pointerEvents: props.disabled ? 'none' : 'auto',
-                    opacity: props.disabled ? 0.5 : 1,
-                }}
-            >
-                <span className={styles['dropdown-label']}>
-                    {props.selectedValue !== null
-                        ? props.items.find(
-                              (i) => i.value === props.selectedValue,
-                          )?.label
-                        : props.selectLabel}
-                </span>
-                <ChevronDown size={14} />
-            </div>
-        </Spicetify.ReactComponent.ContextMenu>
+            {props.selectedValue === null && (
+                <option value={undefined} disabled selected={true}>
+                    {props.selectLabel}
+                </option>
+            )}
+
+            {props.items.map((item) => (
+                <option key={item.value} value={item.value}>
+                    {item.label}
+                </option>
+            ))}
+        </select>
     );
 }
