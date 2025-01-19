b9b6dd26ad105e1fe3b34c694554243708f50e47
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-01-19 18:06:35 +0100

    feat(playlist-maker): show result details in node

--- custom-apps/playlist-maker/src/components/nodes/result/ResultNode.tsx
+++ custom-apps/playlist-maker/src/components/nodes/result/ResultNode.tsx
@@ -5,10 +5,19 @@ import { Node } from '../shared/Node';
 import { NodeContent } from '../shared/NodeContent';
 import { type BaseNodeData } from 'custom-apps/playlist-maker/src/models/nodes/node-processor';
 import { NodeTitle } from '../shared/NodeTitle';
+import { TextComponent } from '@shared/components/ui/TextComponent/TextComponent';
+import useAppStore from 'custom-apps/playlist-maker/src/stores/store';
+import { useShallow } from 'zustand/react/shallow';
 
 export function ResultNode(
     props: Readonly<NodeProps<BaseNodeData>>,
 ): JSX.Element {
+    const { result } = useAppStore(
+        useShallow((state) => ({
+            result: state.result,
+        })),
+    );
+
     return (
         <Node isExecuting={props.data.isExecuting}>
             <NodeHeader
@@ -18,6 +27,30 @@ export function ResultNode(
             />
             <NodeContent>
                 <NodeTitle title="Final playlist" />
+
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '8px',
+                    }}
+                >
+                    <TextComponent elementType="p">
+                        Number of tracks in result:
+                    </TextComponent>
+                    {result.length === 0 && (
+                        <TextComponent elementType="p">-</TextComponent>
+                    )}
+                    {result.length !== 0 && (
+                        <TextComponent
+                            elementType="p"
+                            weight="bold"
+                            paddingBottom="0"
+                        >
+                            {result.length}
+                        </TextComponent>
+                    )}
+                </div>
             </NodeContent>
             <Handle
                 type="target"
