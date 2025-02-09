e8c4060a7b1b5b7956490363b20833fdfc580ea2
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-02-09 17:13:55 +0100

    fix(shared): fix play button size

--- libs/shared/src/components/ui/PlayButton.tsx
+++ libs/shared/src/components/ui/PlayButton.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { getTranslation } from '@shared/utils/translations.utils';
 import { SpotifyIcon } from '@shared/components/ui/SpotifyIcon/SpotifyIcon';
+import { getTranslation } from '@shared/utils/translations.utils';
+import React from 'react';
 
 export type Props = {
     size: Spicetify.ReactComponent.ButtonProps['buttonSize'];
@@ -9,9 +9,20 @@ export type Props = {
 };
 
 export function PlayButton(props: Readonly<Props>): JSX.Element {
+    let legacySize: Spicetify.ReactComponent.ButtonProps['size'] | undefined;
+
+    if (props.size === 'lg') {
+        legacySize = 'large';
+    } else if (props.size === 'md') {
+        legacySize = 'medium';
+    } else if (props.size === 'sm') {
+        legacySize = 'small';
+    }
+
     return (
         <Spicetify.ReactComponent.ButtonPrimary
             aria-label={getTranslation(['play'])}
+            size={legacySize}
             buttonSize={props.size}
             disabled={props.disabled}
             onClick={(e: any) => {
