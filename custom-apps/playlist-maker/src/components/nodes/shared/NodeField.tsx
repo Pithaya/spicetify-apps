51f39cd3e3f4c4c35a6c2bff2c1e592d0964bbd5
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-02-09 02:10:21 +0100

    feat(playlist-maker): tooltip icon

--- custom-apps/playlist-maker/src/components/nodes/shared/NodeField.tsx
+++ custom-apps/playlist-maker/src/components/nodes/shared/NodeField.tsx
@@ -1,9 +1,8 @@
 import { TextComponent } from '@shared/components/ui/TextComponent/TextComponent';
+import { CircleHelp } from 'lucide-react';
 import React, { type PropsWithChildren } from 'react';
 import { type FieldError } from 'react-hook-form';
 import { InputError } from '../../inputs/InputError';
-
-// TODO: help icon for the tooltip
 
 export type Props = PropsWithChildren<{
     label: string;
@@ -12,21 +11,24 @@ export type Props = PropsWithChildren<{
 }>;
 
 export function NodeField(props: Readonly<Props>): JSX.Element {
-    const label = (
-        <TextComponent elementType="small">{props.label}</TextComponent>
-    );
-
     return (
         <label className="flex flex-row items-start justify-between gap-5">
-            {props.tooltip && (
-                <Spicetify.ReactComponent.TooltipWrapper
-                    label={props.tooltip}
-                    showDelay={100}
-                >
-                    {label}
-                </Spicetify.ReactComponent.TooltipWrapper>
-            )}
-            {!props.tooltip && label}
+            <div className="flex items-center gap-1">
+                <TextComponent elementType="small">{props.label}</TextComponent>
+                {props.tooltip && (
+                    <Spicetify.ReactComponent.TooltipWrapper
+                        label={props.tooltip}
+                        showDelay={100}
+                    >
+                        <CircleHelp
+                            size={12}
+                            strokeWidth={1.5}
+                            className="cursor-help"
+                        />
+                    </Spicetify.ReactComponent.TooltipWrapper>
+                )}
+            </div>
+
             <div className="w-[200px]">
                 {props.children}
                 <InputError error={props.error} />
