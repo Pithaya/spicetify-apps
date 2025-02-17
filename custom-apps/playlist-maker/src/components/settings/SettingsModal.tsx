64b499c96de3812e9fd1498ce93a1b111a8fc853
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-02-17 21:23:03 +0100

    ci(playlist-maker): add workflow

--- custom-apps/playlist-maker/src/components/settings/SettingsModal.tsx
+++ custom-apps/playlist-maker/src/components/settings/SettingsModal.tsx
@@ -1,5 +1,14 @@
+import { TextComponent } from '@shared/components/ui/TextComponent/TextComponent';
 import React from 'react';
 
 export function SettingsModal(): JSX.Element {
-    return <div>TODO</div>;
+    return (
+        <TextComponent
+            elementType="p"
+            semanticColor="textSubdued"
+            fontSize="small"
+        >
+            Coming soon
+        </TextComponent>
+    );
 }
