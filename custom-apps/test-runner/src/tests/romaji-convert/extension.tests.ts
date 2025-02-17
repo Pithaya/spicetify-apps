52e9cb9b4fa3c5ce1401c88b828570eebde4d6f8
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-02-17 21:25:04 +0100

    feat(test-runner): add empty test cases

---
+++ custom-apps/test-runner/src/tests/romaji-convert/extension.tests.ts
@@ -0,0 +1,28 @@
+import type { Test } from 'custom-apps/test-runner/src/models/test';
+
+export const tests: Test[] = [
+    {
+        name: 'Context menu on track should not have item if not japanese',
+        test: () => {
+            throw new Error('Not implemented');
+        },
+    },
+    {
+        name: 'Track name should be converted to romaji',
+        test: () => {
+            throw new Error('Not implemented');
+        },
+    },
+    {
+        name: 'Settings with no local storage should have default values',
+        test: () => {
+            throw new Error('Not implemented');
+        },
+    },
+    {
+        name: 'Changing settings should update local storage and context menu',
+        test: () => {
+            throw new Error('Not implemented');
+        },
+    },
+];
