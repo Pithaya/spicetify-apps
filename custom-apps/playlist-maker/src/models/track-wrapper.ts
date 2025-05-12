d8de4ae211cdacda321c1d31a73489d16e1349ba
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-05-12 23:00:40 +0200

    refactor(playlist-maker): refactor query

--- custom-apps/playlist-maker/src/models/track-wrapper.ts
+++ custom-apps/playlist-maker/src/models/track-wrapper.ts
@@ -9,6 +9,9 @@ import type { LibraryAPITrack } from '@shared/platform/library';
 import type { LocalTrack } from '@shared/platform/local-files';
 import type { AdditionalTrackData, WorkflowTrack } from './workflow-track';
 
+/**
+ * Wrapper for a track that implements the ITrack interface.
+ */
 export class TrackWrapper implements ITrack {
     public get uri(): string {
         return this.backingTrack.uri;
