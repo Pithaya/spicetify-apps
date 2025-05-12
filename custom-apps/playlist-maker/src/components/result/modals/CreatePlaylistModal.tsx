c67f0cd11e30348074034dcd4badb9906f01d7d0
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-05-13 00:31:14 +0200

    fix(playlist-maker): fix create playlist modal

--- custom-apps/playlist-maker/src/components/result/modals/CreatePlaylistModal.tsx
+++ custom-apps/playlist-maker/src/components/result/modals/CreatePlaylistModal.tsx
@@ -20,11 +20,6 @@ const FormSchema = z.object({
 
 type Form = z.infer<typeof FormSchema>;
 
-const defaultValues: Form = {
-    playlistName: 'My playlist',
-    parentFolder: undefined,
-};
-
 export function CreatePlaylistModal(): JSX.Element {
     const [isCreating, setIsCreating] = React.useState(false);
     const [folders, setFolders] = React.useState<
@@ -45,7 +40,10 @@ export function CreatePlaylistModal(): JSX.Element {
     } = useForm<Form>({
         mode: 'onChange',
         defaultValues: {
-            ...defaultValues,
+            playlistName: '',
+            parentFolder: undefined,
+        },
+        values: {
             playlistName: workflowName,
         },
         resolver: zodResolver(FormSchema),
