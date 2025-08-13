87f853bb55256ada01820bfaf2403418cb4c49a8
Pithaya <19533412+Pithaya@users.noreply.github.com>
2025-08-13 18:31:06 +0200

    fix(shared): fix context menu

--- libs/shared/src/components/menus/Menu.tsx
+++ libs/shared/src/components/menus/Menu.tsx
@@ -2,7 +2,9 @@ import React, { type PropsWithChildren } from 'react';
 
 export function Menu(props: Readonly<PropsWithChildren>): JSX.Element {
     return (
-        <Spicetify.ReactComponent.Menu className="encore-dark-theme main-contextMenu-menu">
+        <Spicetify.ReactComponent.Menu
+            style={{ backgroundColor: '(--spice-card)' }}
+        >
             {props.children}
         </Spicetify.ReactComponent.Menu>
     );
