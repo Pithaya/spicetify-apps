2172344b407dc59b359ccfb0318247307e750313
Pithaya <19533412+Pithaya@users.noreply.github.com>
2026-05-22 18:15:35 +0200

    feat: debug menu for platform proxies

--- dev-extensions/list-icons/src/app.tsx
+++ dev-extensions/list-icons/src/app.tsx
@@ -1,10 +1,12 @@
 import { listIcons } from '@shared/debug/list-icons';
+import { registerProxyMenuItem } from '@shared/debug/register-proxy';
 import { waitForSpicetify } from '@shared/utils/spicetify-utils';
 
 async function main(): Promise<void> {
     await waitForSpicetify();
 
     listIcons();
+    registerProxyMenuItem();
 }
 
 export default main;
--- libs/shared/src/debug/register-proxy.ts
+++ libs/shared/src/debug/register-proxy.ts
@@ -1,29 +1,41 @@
+import type { Platform } from '@shared/platform/platform';
+
 function createProxyHandler<T extends object>(
     objectName: string,
+    propertiesToIgnore: string[] = [],
 ): ProxyHandler<T> {
     const handler = {
-        get(target: T, property: string, receiver: any) {
+        get(target: T, property: string, receiver: unknown) {
             const targetValue = Reflect.get(target, property, receiver);
 
             if (typeof targetValue === 'function') {
                 return function (...args: unknown[]) {
-                    const result = targetValue.apply(this, args);
-                    console.log(
-                        `[${objectName}] - CALL`,
-                        property,
-                        args,
-                        `-->`,
-                        result,
-                    );
+                    const result: unknown = targetValue.apply(this, args);
+
+                    // eslint-disable-next-line sonarjs/argument-type
+                    if (!propertiesToIgnore.includes(property)) {
+                        console.log(
+                            `[${objectName}] - CALL`,
+                            property,
+                            args,
+                            `-->`,
+                            result,
+                        );
+                    }
+
                     return result;
                 };
             } else {
-                console.log(
-                    `[${objectName}] - GET`,
-                    property,
-                    '-->',
-                    targetValue,
-                );
+                // eslint-disable-next-line sonarjs/argument-type
+                if (!propertiesToIgnore.includes(property)) {
+                    console.log(
+                        `[${objectName}] - GET`,
+                        property,
+                        '-->',
+                        targetValue,
+                    );
+                }
+
                 return targetValue;
             }
         },
@@ -37,11 +49,20 @@ function createProxyHandler<T extends object>(
  * @param object The object to spy on.
  * @param objectName A name to be printed to the console.
  */
-export function registerProxy<T>(object: T, objectName: string): void {
+export function registerProxy<T>(
+    object: T,
+    objectName: string,
+    propertiesToIgnore: string[] = [],
+): void {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-argument
     const prototype = Object.create(Object.getPrototypeOf(object));
     Object.setPrototypeOf(
         object,
-        new Proxy(prototype, createProxyHandler(objectName)),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        new Proxy(
+            prototype,
+            createProxyHandler(objectName, propertiesToIgnore),
+        ),
     );
 
     console.log(`Registered proxy for ${objectName}.`);
@@ -50,30 +71,44 @@ export function registerProxy<T>(object: T, objectName: string): void {
 /**
  * Wrap all APIs exposed by Spicetify.Platform with a proxy.
  */
-export function registerPlatformProxies(): void {
-    for (const [name, api] of Object.entries(Spicetify.Platform)) {
-        registerProxy(api, name);
+export function registerPlatformProxies(
+    propertiesToIgnore: string[] = [],
+): void {
+    for (const [name, api] of Object.entries(Spicetify.Platform as Platform)) {
+        registerProxy(api, name, propertiesToIgnore);
     }
 }
 
-export function registerServicesProxies(): void {
-    const servicesMap = new Map<string, any>();
+export function registerServicesProxies(
+    propertiesToIgnore: string[] = [],
+): void {
+    const servicesMap = new Map<string, unknown>();
 
     for (const [platformName, platformApi] of Object.entries(
-        Spicetify.Platform,
+        Spicetify.Platform as Platform,
     )) {
-        for (const [name, service] of Object.entries(platformApi as any).filter(
-            ([n, s]) => n.startsWith('_'),
-        )) {
+        for (const [name, service] of Object.entries(
+            platformApi as Record<string, unknown>,
+        ).filter(([n, s]) => n.startsWith('_'))) {
             const fullName = `${platformName}.${name}`;
             if (!servicesMap.has(name)) {
                 servicesMap.set(name, service);
                 try {
-                    registerProxy(service, fullName);
+                    registerProxy(service, fullName, propertiesToIgnore);
                 } catch {}
             }
         }
     }
 
     console.log(servicesMap);
+}
+
+export function registerProxyMenuItem(): void {
+    new Spicetify.ContextMenu.Item(
+        'Register platform proxies',
+        () => {
+            registerPlatformProxies(['resolve', 'resolveImpl']);
+        },
+        () => true,
+    ).register();
 }
